b3a219440488b75f1d229e3334b2d4666b6a9f1d
fbtjdwls04 <fbtjdwls04@gmail.com>
2023-04-14 16:40:10 +0900

    난수 회전각 적용

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 
 function App() {
-  let [num, setNum] = useState(0);
-  let name = "";
-  if(num == 1){
-    name = "moving";
-  }
-  else if(num == 2){
-    name = "";
-    num = 0;
-  }
+  let [name, setNum] = useState('');
   const rNum = (min, max) => Math.random() * (min-max) + min;
-  console.log(rNum(3600,3960));
+  const rn = {
+    rotate: `${rNum(3960,4320)}deg`,
+    transition: 'rotate 10s'
+  }
+  function twinActive(){
+    if(name == 'moving'){
+      return rn;
+    }
+  }
+
   return (
     <>
-      <div className={"test " + name} onClick={()=>setNum(num+1)}>
-        <div className={"circle red"}></div>
+      <div className={"arrow red"}>
+      </div>
+      <div className={"circle " + name} style={twinActive()}>
+        <div className="menu">응애</div>
+      </div>
+      <div className="btn-box">
+        <button className="btn-type run" onClick={()=> setNum("moving")}>시작하기</button>
+        <button className="btn-type stop" onClick={()=> setNum("stop")}>정지하기</button>
       </div>
     </>
   );
